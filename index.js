87fe317297c7e5298d0e15234cf67c003bf1b295
Marcelo Guazzardo <mguazzardo76@gmail.com>
2020-11-19 16:39:23 -0300

    subiendo

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 
+const port = 8080
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
  
-var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server = app.listen(port, function () {
-  //port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  console.log('Example app listening at http://' +ip+ '/' + port);
+//var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+app.listen(port, () => {
+
+    console.log(`Server listening on the port::::::${port}`);
+
 });
+
+
