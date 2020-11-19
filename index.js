e4bcd7dc730a253c690e9880f77d736687636b66
Marcelo Guazzardo <mguazzardo76@gmail.com>
2020-11-19 15:49:30 -0300

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
  
 
 var server = app.listen(3000, function () {
-  ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  console.log('Example app listening at http://%s:%s', host, port);
+  var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  console.log('Example app listening at http://0.0.0.0' + port);
 });
