7d31ed2d206726624e3065b96612409225ce68cb
Marcelo Guazzardo <mguazzardo76@gmail.com>
2020-11-19 16:30:33 -0300

    subiendo

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
  
-
-var server = app.listen(3000, function () {
-  ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server = app.listen(port, function () {
+  //port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   console.log('Example app listening at http://' +ip+ '/' + port);
 });
