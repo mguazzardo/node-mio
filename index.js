f0291ccdfa36480064b7ad7e4545b1507f4b1474
mguazzardo <mguazzardo76@gmail.com>
2021-10-11 20:23:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+var mysql = require('mysql');
 var express = require('express');
 var app = express();
 
@@ -5,9 +7,29 @@ var app = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var app = express();
+var port = process.env.PORT || 8080;
+var responseStr = "MySQL Data:";
+
+app.get('/',function(req,res){
+   
+   var mysqlHost = process.env.MYSQL_HOST || 'mariadb';
+   var mysqlPort = process.env.MYSQL_PORT || '3306';
+   var mysqlUser = process.env.MYSQL_USER || 'user1';
+   var mysqlPass = process.env.MYSQL_PASS || 'master';
+   var mysqlDB   = process.env.MYSQL_DB   || 'test';
+
+   var connectionOptions = {
+     host: mysqlHost,
+     port: mysqlPort,
+     user: mysqlUser,
+     password: mysqlPass,
+     database: mysqlDB
+   };
+
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
  
 
 app.listen(port, () => {
