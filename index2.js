4e54f4c7107d4f20ea60708ac252983e4bf2c484
mguazzardo <mguazzardo76@gmail.com>
2021-10-11 20:52:09 -0300

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,41 @@
+'use strict';
+var mysql = require('mysql');
+var express = require('express');
+var app = express();
+
+//const port = 8080
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var app = express();
+var port = process.env.PORT || 8080;
+var responseStr = "MySQL Data:";
+
+
+app.get('/',function(req,res){
+   
+   var mysqlHost = process.env.MYSQL_HOST || 'mariadb';
+   var mysqlPort = process.env.MYSQL_PORT || '3306';
+   var mysqlUser = process.env.MYSQL_USER || 'user1';
+   var mysqlPass = process.env.MYSQL_PASS || 'master';
+   var mysqlDB   = process.env.MYSQL_DB   || 'test';
+
+   var connectionOptions = {
+     host: mysqlHost,
+     port: mysqlPort,
+     user: mysqlUser,
+     password: mysqlPass,
+     database: mysqlDB
+   };
+   res.send('Hello World!');
+});
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
+ 
+
+app.listen(port, () => {
+
+    console.log(`Server listening on the port::::::${port}`);
+
+});
