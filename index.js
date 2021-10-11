a306972dfc439e818906c9e58863ab4c1fc918a1
mguazzardo <mguazzardo76@gmail.com>
2021-10-11 20:42:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/',function(req,res){
      password: mysqlPass,
      database: mysqlDB
    };
+   res.send('Hello World!');
 });
 //app.get('/', function (req, res) {
 //  res.send('Hello World!');
